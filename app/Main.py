30cd698f7242341bbb5b40ace2bdd8c142b5ab95
ahmad sameet <ahmaadsameet@gmail.com>
2026-03-27 23:25:15 +0500

    added routes in main code file

--- app/Main.py
+++ app/Main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from app.api.product.product_routes import product 
+from app.api.user.user_routes import user 
+from app.api.order.order_routes import order 
 
-app=FastAPI()
 
-@app.GET("/")
+app=FastAPI() 
+
+app.include_router(product)
+app.include_router(user)
+app.include_router(order)
+
+
+@app.get("/")
 def read_root():
-    return{"hello":"world"}
+    return{"hello":"api running"}
 
 
 
