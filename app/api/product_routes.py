8801c275bec0e0d879777f39a3b27f55b9fa8d5b
ahmad sameet <ahmaadsameet@gmail.com>
2026-03-27 22:09:11 +0500

    added user routes

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter
 router = APIRouter()
 
-@router.get("products")
+@router.get("/products")
 def get_product():
     return["item","item"]
 
-router.post("products")
+router.post("/products")
 def create_product():
     return{"message":"product created"}
- 
+
+router.put("/products")
+def update_product():
+    return{"message":"product updated"}
---
+++ app/api/user_routes.py
@@ -0,0 +1,19 @@
+from fastapi import user_router
+
+user = user_router()
+
+user.get("/users")
+def get_user():
+    return["user","user"]
+
+user.post("/users")
+def post_user():
+    return{"message":"user created"}
+
+user.put("/users")
+def update_user():
+    return{"message":"user updated"}
+
+user.delete("/users")
+def delete_user():
+    return{"message":"user deleted"}
