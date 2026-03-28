df6d772a67bc74be11ec0283a2101c7c8f5f3f3a
ahmad sameet <ahmaadsameet@gmail.com>
2026-03-28 21:52:50 +0500

    fix function name

--- app/api/product/product_routes.py
+++ app/api/product/product_routes.py
@@ -2,10 +2,10 @@ from fastapi import APIRouter
 router = APIRouter()
 
 @router.get("/products")
-def get_product():
+def get_products():
     return["item","item"]
 
-@router.get("/products/{id}")
+@router.get("/products,/{id}")
 def get_product(id:int):
     return{"item_id":id,"name":"item name"}
 
