efce439336072c9d5b84afcfa86a3f731beb9642
ahmad sameet <ahmaadsameet@gmail.com>
2026-03-31 11:22:23 +0500

    added user ervice file

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,1 +1,15 @@
-class
+class userService:
+    def __init__(self, user_repository):
+        self.user_repository = user_repository
+
+    def create_user(self, user_data):
+        return self.user_repository.create(user_data)
+
+    def get_user(self, user_id):
+        return self.user_repository.get_by_id(user_id)
+
+    def update_user(self, user_id, user_data):
+        return self.user_repository.update(user_id, user_data)
+
+    def delete_user(self, user_id):
+        return self.user_repository.delete(user_id)
